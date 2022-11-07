77574edc2596e23eed96314362cefd50b1db3674
yiru <yi_ru@qq.com>
2022-11-07 16:59:37 +0800

    2

---
+++ src/main/java/yi/git/GitTest.java
@@ -0,0 +1,14 @@
+package yi.git;
+
+/**
+ * @aythor yi
+ * @data 2022/11/7 itime
+ * @Description
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello github");
+        System.out.println("hello github");
+    }
+}
