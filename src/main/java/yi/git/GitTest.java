b46d1203da4a5d74c83988c016cdb37544c86464
yiru <yi_ru@qq.com>
2022-11-08 09:47:31 +0800

    master 1

--- src/main/java/yi/git/GitTest.java
+++ src/main/java/yi/git/GitTest.java
@@ -11,5 +11,6 @@ public class GitTest {
         System.out.println("hello github");
         System.out.println("hello github");
         System.out.println("hello hotFix");
+        System.out.println("master test");
     }
 }
